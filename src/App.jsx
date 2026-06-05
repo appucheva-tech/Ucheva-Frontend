c44a5fc591f7e115b9961f7ac3a92a3c09f34225
mohyis <mohyis007@gmail.com>
2026-06-05 12:53:39 +0100

    refactored app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -64,18 +64,30 @@ import Step1 from "./features/Unboarding/Pages/Step1";
 import Step2 from "./features/Unboarding/Pages/Step2";
 import Step3 from "./features/Unboarding/Pages/Step3";
 import Success from "./features/Unboarding/Pages/Success";
+import SignUp from "./features/auth/SignUp";
+import Login from "./features/auth/Login";
+import VerifyEmail from "./features/auth/VerifyEmail";
+import ForgetPassword from "./features/auth/ForgetPassword";
+import InputCode from "./features/auth/InputCode";
+import ResetPassword from "./features/auth/ResetPassword";
 
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<Step1 />} />
-         <Route path="/step2" element={<Step2 />} />
-         <Route path="/step3" element={<Step3 />} />
-       <Route path="/success" element={<Success />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+              <Route path="/" element={<Step1 />} />
+            <Route path="/step2" element={<Step2 />} />
+            <Route path="/step3" element={<Step3 />} />
+          <Route path="/success" element={<Success />} />
+            <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+            <Route path="/verifyEmail" element={<VerifyEmail />} />
+            <Route path="/forgetpassword" element={<ForgetPassword />} />
+            <Route path="/inputCode" element={<InputCode />} />
+            <Route path="/resetpassword" element={<ResetPassword />} />
+        </Routes>
+      </BrowserRouter>
   );
 };
 
