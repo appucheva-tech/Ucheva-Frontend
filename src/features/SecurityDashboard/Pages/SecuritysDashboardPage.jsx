c2c7df0ef8e8eec9dafb41a2a0d6dc4d0c0a49ad
simisola-tech <omoniyiomosimisola18@gmail.com>
2026-06-08 23:33:45 +0100

    i built the securitydashboard

--- src/features/SecurityDashboard/Pages/SecuritysDashboardPage.jsx
+++ src/features/SecurityDashboard/Pages/SecuritysDashboardPage.jsx
@@ -13,22 +13,96 @@ const SecuritysDashboardPage = () => {
         <div className="check-in-card">
           <div className="qr-icon">
             <svg
-              viewBox="0 0 24 24"
-              width="64"
-              height="64"
-              fill="none"
               xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              viewBox="0 0 50 50"
+              fill="none"
             >
-              <rect x="2" y="2" width="8" height="8" fill="#0052CC" />
-              <rect x="14" y="2" width="8" height="8" fill="#0052CC" />
-              <rect x="2" y="14" width="8" height="8" fill="#0052CC" />
-              <rect x="6" y="6" width="2" height="2" fill="white" />
-              <rect x="18" y="6" width="2" height="2" fill="white" />
-              <rect x="6" y="18" width="2" height="2" fill="white" />
-              <rect x="14" y="14" width="2" height="2" fill="#0052CC" />
-              <rect x="18" y="14" width="2" height="2" fill="#0052CC" />
-              <rect x="14" y="18" width="2" height="2" fill="#0052CC" />
-              <rect x="18" y="18" width="2" height="2" fill="#0052CC" />
+              <path
+                d="M14.5833 6.25H8.33333C7.18274 6.25 6.25 7.18274 6.25 8.33333V14.5833C6.25 15.7339 7.18274 16.6667 8.33333 16.6667H14.5833C15.7339 16.6667 16.6667 15.7339 16.6667 14.5833V8.33333C16.6667 7.18274 15.7339 6.25 14.5833 6.25Z"
+                stroke="#0062F6"
+                stroke-width="3.33333"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M41.6673 6.25H35.4173C34.2667 6.25 33.334 7.18274 33.334 8.33333V14.5833C33.334 15.7339 34.2667 16.6667 35.4173 16.6667H41.6673C42.8179 16.6667 43.7507 15.7339 43.7507 14.5833V8.33333C43.7507 7.18274 42.8179 6.25 41.6673 6.25Z"
+                stroke="#0062F6"
+                stroke-width="3.33333"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M14.5833 33.334H8.33333C7.18274 33.334 6.25 34.2667 6.25 35.4173V41.6673C6.25 42.8179 7.18274 43.7507 8.33333 43.7507H14.5833C15.7339 43.7507 16.6667 42.8179 16.6667 41.6673V35.4173C16.6667 34.2667 15.7339 33.334 14.5833 33.334Z"
+                stroke="#0062F6"
+                stroke-width="3.33333"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M43.7507 33.334H37.5007C36.3956 33.334 35.3358 33.773 34.5544 34.5544C33.773 35.3358 33.334 36.3956 33.334 37.5007V43.7507"
+                stroke="#0062F6"
+                stroke-width="3.33333"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M43.75 43.75V43.7708"
+                stroke="#0062F6"
+                stroke-width="3.33333"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M25.0007 14.584V20.834C25.0007 21.9391 24.5617 22.9989 23.7803 23.7803C22.9989 24.5617 21.9391 25.0007 20.834 25.0007H14.584"
+                stroke="#0062F6"
+                stroke-width="3.33333"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M6.25 25H6.27083"
+                stroke="#0062F6"
+                stroke-width="3.33333"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M25 6.25H25.0208"
+                stroke="#0062F6"
+                stroke-width="3.33333"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M25 33.334V33.3548"
+                stroke="#0062F6"
+                stroke-width="3.33333"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M33.334 25H35.4173"
+                stroke="#0062F6"
+                stroke-width="3.33333"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M43.75 25V25.0208"
+                stroke="#0062F6"
+                stroke-width="3.33333"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M25 43.7493V41.666"
+                stroke="#0062F6"
+                stroke-width="3.33333"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
             </svg>
           </div>
           <h2 className="check-in-title">Checked In Succesful</h2>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+
+    allowedHosts: [
+      ".nip.io",
+      "127.0.0.1.nip.io",
+    //  "app-modelak-college.127.0.0.1.nip.io",
+    ],
+  },
+});
