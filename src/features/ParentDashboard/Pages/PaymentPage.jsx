2f0e99db9691e2aa311354b85bd7623572d4dfb0
aninkud2 <adetadeservices@gmail.com>
2026-06-27 17:07:25 +0100

    conflict resolve

--- src/features/ParentDashboard/Pages/PaymentPage.jsx
+++ src/features/ParentDashboard/Pages/PaymentPage.jsx
@@ -77,15 +77,7 @@ const PaymentPage = () => {
   if (error)
     return <div className="payment-page flex-center-view">{error}</div>;
   if (!selectedStudent)
-<<<<<<< HEAD
     return <div className="payment-page flex-center-view">No student selected. Please select a student to continue.</div>;
-=======
-    return (
-      <div className="payment-page flex-center-view">
-        No student selected. Please select a student to continue.
-      </div>
-    );
->>>>>>> 9b88fc74a336f52646724175afd5a02fbd67b80e
 
   const total = paymentData.totalFee;
   const amountNow =
